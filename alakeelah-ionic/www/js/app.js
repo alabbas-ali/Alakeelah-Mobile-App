e805e542f0be3b868af332a74bbe954c57e627fc
Yahya Alfayad <yahya.alfayad@gmail.com>
2015-06-08 12:52:08 +0300

    try/catch added to the first ionicPlatform.ready in app.js

--- alakeelah-ionic/www/js/app.js
+++ alakeelah-ionic/www/js/app.js
@@ -13,20 +13,24 @@ angular
 		.run(
 				function($ionicPlatform, $translate, $state, $ionicHistory) {
 					$ionicPlatform.ready(function() {
-						alert('device ready called in app.js');
 						// Hide the accessory bar by default (remove
 						// this to
 						// show the
 						// accessory
 						// bar above the keyboard
 						// for form inputs)
-						if (window.cordova && window.cordova.plugins.Keyboard) {
-							cordova.plugins.Keyboard
-									.hideKeyboardAccessoryBar(true);
-						}
-						if (window.StatusBar) {
-							// org.apache.cordova.statusbar required
-							StatusBar.styleDefault();
+						try {
+							if (window.cordova
+									&& window.cordova.plugins.Keyboard) {
+								cordova.plugins.Keyboard
+										.hideKeyboardAccessoryBar(true);
+							}
+							if (window.StatusBar) {
+								// org.apache.cordova.statusbar required
+								StatusBar.styleDefault();
+							}
+						} catch (e) {
+							console.log(e);
 						}
 
 					});
