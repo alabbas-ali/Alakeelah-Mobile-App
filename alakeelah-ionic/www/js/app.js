e547e82ef089b8d37cbbffcb78faa7a8a456d75e
abass <abass@WIN-949SM4EVIL9>
2015-06-15 10:36:50 +0300

    Nothing important

--- alakeelah-ionic/www/js/app.js
+++ alakeelah-ionic/www/js/app.js
@@ -8,7 +8,7 @@ angular
 		.module(
 				'starter',
 				[ 'ionic', 'starter.controllers', 'ngCordova',
-						'pascalprecht.translate' , 'ksSwiper' ])
+						'pascalprecht.translate' , 'ksSwiper'])
 
 		.run(
 				function($ionicPlatform, $translate, $state, $ionicHistory) {
